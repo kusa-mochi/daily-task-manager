1b02166a809e5b4e3c1945c92a602168f536f55d
kusa-mochi <whoatemyapplepie@gmail.com>
2021-11-13 22:17:45 +0900

    electronのビルド出力形式を設定

--- daily-task-manager/vue.config.js
+++ daily-task-manager/vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "daily-task-manager",
+        appId: "com.mochi.dailytaskmanager",
+        win: {
+          target: [
+            {
+              target: 'portable',
+              arch: ['x64']
+            }
+          ]
+        }
+      }
+    }
+  },
   transpileDependencies: ["vuetify"],
 };
