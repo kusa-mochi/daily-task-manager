e1e4dd3242959781507c97cbd45bad61306a215b
kusa-mochi <whoatemyapplepie@gmail.com>
2021-11-16 21:46:44 +0900

    サイドメニューのワイヤーフレームを実装。

--- daily-task-manager/src/App.vue
+++ daily-task-manager/src/App.vue
@@ -1,41 +1,40 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
+      <v-app-bar-nav-icon
+        @click.stop="isSideMenuShown = !isSideMenuShown"
+      ></v-app-bar-nav-icon>
       <router-view />
     </v-main>
+    <v-navigation-drawer v-model="isSideMenuShown" absolute temporary>
+      <v-list>
+        <v-list-item-group v-model="selectedSideMenuIndex">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>file-plus</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title> 新規作成 </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title> 開く </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title> 保存 </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title> 名前を付けて保存 </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title> ヘルプ </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title> バージョン情報 </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
 
@@ -44,9 +43,11 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "App",
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      isSideMenuShown: false,
+      selectedSideMenuIndex: null,
+    };
+  },
 });
 </script>
